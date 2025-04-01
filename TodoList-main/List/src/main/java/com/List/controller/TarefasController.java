c82f563252247d8e2afb5958b10ea59cea890775
AnaaPds <anapauladantas1804@gmail.com>
2025-04-01 11:46:29 -0300

    Documentando TarefasController.java

--- TodoList-main/List/src/main/java/com/List/controller/TarefasController.java
+++ TodoList-main/List/src/main/java/com/List/controller/TarefasController.java
@@ -10,23 +10,28 @@ import java.util.List;
 @RequestMapping("/tarefas")
 public class TarefasController {
 
+    // Declara o serviço de tarefas
     public TarefasService tarefasService;
 
+    // Construtor que injeta o serviço de tarefas
     public TarefasController(TarefasService tarefaService) {
         this.tarefasService = tarefaService;
     }
 
+    // Método GET para listar todas as tarefas
     @GetMapping
     public List<Tarefas> listarTarefas() {
         return tarefasService.listarTarefas();
     }
 
+    // Método GET para buscar uma tarefa pelo ID
     @GetMapping("/{id}")
     public Tarefas buscarTarefaPorId(@PathVariable Long id) {
         return tarefasService.buscarTarefaPorId(id);
     }
 
 
+    // Método POST para criar uma nova tarefa
     @PostMapping
     public Tarefas criarTarefa(@RequestBody Tarefas tarefa) {
         return tarefasService.salvarTarefa(tarefa);
