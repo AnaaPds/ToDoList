7fada0a46010d4b8217d29c40b43742233bdcd28
AnaaPds <anapauladantas1804@gmail.com>
2025-04-01 13:04:25 -0300

    Documentando TarefasService.java

--- TodoList-main/List/src/main/java/com/List/service/TarefasService.java
+++ TodoList-main/List/src/main/java/com/List/service/TarefasService.java
@@ -9,21 +9,26 @@ import java.util.List;
 @Service
 public class TarefasService {
 
+    // Repositório para acessar as tarefas no banco de dados
     public TarefasRepository tarefasRepository;
 
+    // Construtor que injeta o repositório de tarefas
     public TarefasService(TarefasRepository tarefaRepository) {
         this.tarefasRepository = tarefaRepository;
     }
 
+    // Método para listar todas as tarefas
     public List<Tarefas> listarTarefas() {
         return tarefasRepository.findAll();
     }
 
+    // Método para buscar uma tarefa por ID
     public Tarefas buscarTarefaPorId(Long id) {
         return tarefasRepository.findById(id).orElseThrow(() -> new RuntimeException("Tarefa não encontrada"));
     }
 
 
+    // Método para salvar uma nova tarefa
     public Tarefas salvarTarefa(Tarefas tarefa) {
         return tarefasRepository.save(tarefa);
     }
