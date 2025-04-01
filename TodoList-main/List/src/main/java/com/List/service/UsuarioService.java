c6a2b4ac06ff1c13da0f4a58f4ed7ddc36d57b03
AnaaPds <anapauladantas1804@gmail.com>
2025-04-01 13:05:52 -0300

    Documentando UsuarioService.java

--- TodoList-main/List/src/main/java/com/List/service/UsuarioService.java
+++ TodoList-main/List/src/main/java/com/List/service/UsuarioService.java
@@ -9,21 +9,26 @@ import java.util.List;
 @Service
 public class UsuarioService {
 
+    // Repositório para acessar os usuários no banco de dados
     public UsuarioRepository usuarioRepository;
 
+    // Construtor que injeta o repositório de usuários
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
+    // Método para listar todos os usuários
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
     }
 
+    // Método para buscar um usuário por ID
     public Usuario buscarUsuarioPorId(Long id) {
         return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
 
 
+    // Método para salvar um novo usuário
     public Usuario salvarUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
